f967e79d6aca9405dfb88f7d6705416b38a4c98a
muhammadilham <muhammadilhamhafizha@gmail.com>
2026-05-29 13:37:26 +0700

    feat(board): update board

--- controllers/board_controller.go
+++ controllers/board_controller.go
@@ -36,10 +36,38 @@ func (c BoardController) CreateBoard(ctx *fiber.Ctx) error {
 	}
 
 	board.OwnerPublicID = userID
-	
+
 	if err := c.services.Create(board); err != nil {
 		return utils.BadRequest(ctx, "Gagal Membuat Board", err.Error())
 	}
 
 	return utils.Success(ctx, "Board Berhasil Dibuat", board)
+}
+
+func (c *BoardController) UpdateBoard(ctx *fiber.Ctx) error {
+	publicID := ctx.Params("id")
+	board := new(models.Board)
+
+	if err := ctx.BodyParser(board); err != nil {
+		return utils.BadRequest(ctx, "Gagal Parsing Data", err.Error())
+	}
+
+	if _, err := uuid.Parse(publicID); err != nil {
+		return utils.BadRequest(ctx, "Invalid ID Format", err.Error())
+	}
+
+	existingBoard, err := c.services.GetByPublicID(publicID)
+	if err != nil {
+		return utils.BadRequest(ctx, "Gagal Mengambil Board", err.Error())
+	}
+
+	board.InternalID = existingBoard.InternalID
+	board.PublicID = existingBoard.PublicID
+	board.OwnerPublicID = existingBoard.OwnerPublicID
+	board.OwnerID = existingBoard.OwnerID
+
+	if err := c.services.Update(board); err != nil {
+		return utils.BadRequest(ctx, "Gagal Update Board", err.Error())
+	}
+	return utils.Success(ctx, "Board Berhasil Diupdate", board)
 }
